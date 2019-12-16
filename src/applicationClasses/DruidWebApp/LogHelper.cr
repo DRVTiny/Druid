fd64470c6fca2b5bae633adee935c4ac1442c3b6
Andrey A. Konovalov <drvtiny@gmail.com>
2019-12-16 14:48:10 +0300

    fix: forgot LogHelper

---
+++ src/applicationClasses/DruidWebApp/LogHelper.cr
@@ -0,0 +1,38 @@
+module DruidWebApp
+  class LogHelper
+    def self.get_logger(file : (IO | String | Nil) = STDERR, log_level = Logger::DEBUG)
+      fh = 
+        case file
+        when IO
+          file
+        when String
+          file == "-" ? STDERR : File.new(file, "a")
+        else
+          STDERR
+        end
+      log_hndl = Logger.new(fh, log_level)
+      log_hndl.progname = AppName.exec_name
+      log_hndl.formatter = Logger::Formatter.new do |severity, datetime, progname, message, io|
+        logsev = severity.unknown? ? "ANY" : severity.to_s
+        io << [
+          datetime.to_s("%H:%M:%S"),
+          datetime.to_s("%d-%m-%Y"),
+          logsev,
+          "pid=#{Process.pid}, fbr=#{Fiber.current.object_id}",
+          message
+        ].join(" | ")       
+      end
+      {log_hndl, fh}
+    end
+  end
+  
+  class KemaLoger < Kemal::LogHandler
+    def initialize(@log_hndl : Logger, @io : IO = STDERR)
+    end
+    
+    def write(message : String)
+      @log_hndl.info(message)
+      @io
+    end
+  end
+end
